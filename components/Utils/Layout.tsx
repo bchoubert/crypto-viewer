b077793d133ac51f32b96401642278a907086f90
Bertrand Choubert <bertrand.choubert@hotmail.fr>
2021-10-18 21:13:55 +0200

    feat(tests-3): topbar - rename 2/2

--- components/Utils/Layout.tsx
+++ components/Utils/Layout.tsx
@@ -13,7 +13,7 @@ import BottomBar from './BottomBar';
 
 import Tabs from '../../models/Tabs';
 
-import TopBar from './Topbar2';
+import TopBar from './Topbar';
 import { NavigationContext } from '../../contexts/NavigationProvider';
 import { ThemeContext } from '../../contexts/ThemeProvider';
 
